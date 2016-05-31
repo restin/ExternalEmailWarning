cac1a4dfd90948078fbb97fffb17782e901ca50a
jamie.hess <jamie.hess@MJ035SUC>
2016-05-31 14:36:38 -0500

    Changed Icon

--- ExternalEmailWarning/SecureEmailForm.Designer.cs
+++ ExternalEmailWarning/SecureEmailForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SecureEmailForm));
             this.label1 = new System.Windows.Forms.Label();
             this.lstEmailAddresses = new System.Windows.Forms.ListBox();
             this.btnSendSecure = new System.Windows.Forms.Button();
@@ -99,6 +100,7 @@
             this.Controls.Add(this.btnSendSecure);
             this.Controls.Add(this.lstEmailAddresses);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SecureEmailForm";
